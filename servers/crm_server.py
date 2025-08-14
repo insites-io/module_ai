fa3b9100a631ca13706f89cbb3826fbefb031144
Enrique Tanada III <enriquetanada@gmail.com>
2025-08-14 13:43:21 +0800

    Fix logging configuration in crm_server.py - Remove FileHandler that tries to write to log file - This was causing permission issues in Cloud Run container - Only use stdout/stderr for logging to avoid file permission errors

--- servers/crm_server.py
+++ servers/crm_server.py
@@ -13,8 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler('crm_server.log')
+        logging.StreamHandler(sys.stdout)
     ]
 )
 logger = logging.getLogger(__name__)
